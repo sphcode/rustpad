a6a4fd09db65950b815223c0003591d17f388ac9
sphcode <puhuasun@gmail.com>
2024-12-18 19:57:26 +0800

    feat: editor

---
+++ src/editor.rs
@@ -0,0 +1,31 @@
+use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+
+pub struct Editor {
+
+}
+
+impl Editor {
+    pub fn default() -> Self {            
+        Editor{}            
+    }
+    pub fn run(&self){
+        enable_raw_mode().unwrap();
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{event:?} \r");
+                    if let Char(c) = event.code {
+                        if c == 'q' {
+                            break;
+                        }
+                    }   
+                },
+                Err(err) => println!("Error: {err}"),
+                _ => ()
+            }
+        }
+        disable_raw_mode().unwrap(); 
+    }
+}
