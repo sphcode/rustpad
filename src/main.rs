14c6c4ab31ffd33d2ed990329ce1003379db87ea
sphcode <puhuasun@gmail.com>
2024-12-21 20:41:22 +0800

    feat: mass in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 use crossterm::event::{Event, KeyCode, KeyEvent};
 use crossterm::{event, terminal};
-use std::time::Duration; /* add this line */
+use std::time::Duration;
 use std::result::Result;
 struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
         terminal::disable_raw_mode().expect("Unable to disable raw mode")
+    }
+}
+
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> Result<KeyEvent, Box<dyn std::error::Error>> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    return Ok(event);
+                }
+            }
+        }
+    }
+}
+
+struct Editor {
+    reader: Reader,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self { reader: Reader }
     }
 }
 
