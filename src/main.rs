ce9c03a32a1767969e289b99788c69d3e18a1464
sphcode <puhuasun@gmail.com>
2024-12-21 20:11:27 +0800

    refactor:  pound is good

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
-#![warn(clippy::all, clippy::pedantic)]
-mod editor;
-use editor::Editor;
+use crossterm::event::{Event, KeyCode, KeyEvent}; /* modify */
+use crossterm::{event, terminal};
+use std::io;
+use std::io::Read;
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Unable to disable raw mode")
+    }
+}
 
 fn main() {
-    Editor::default().run();
+    let _clean_up = CleanUp;
+    terminal::enable_raw_mode().expect("Could not turn on Raw mode");
+    /* add the following */
+    loop {
+        if let Event::Key(event) = event::read().expect("Failed to read line") {
+            match event {
+                KeyEvent {
+                    code: KeyCode::Char('q'),
+                    modifiers: event::KeyModifiers::NONE, ..
+                } => break,
+                _ => {
+                    //todo
+                }
+            }
+            println!("{:?}\r", event);
+        };
+    }
+    /* end */
+    let mut buf = [0; 1];
+    while io::stdin().read(&mut buf).expect("Failed to read") == 1 && buf != [b'q'] {
+        let character = buf[0] as char;
+        if character.is_control() {
+            println!("{}\r", character as u8)
+        } else {
+            println!("{}\r", character)
+        }
+    }
 }
